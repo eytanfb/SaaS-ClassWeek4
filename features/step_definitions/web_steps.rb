d60c3900e1094e8b4d1c31aee81835029b5daa3f
eytanfb <eytanfb@gmail.com>
2012-03-16 11:07:36 +0200

    commited part 1

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -31,6 +31,20 @@ module WithinHelpers
 end
 World(WithinHelpers)
 
+# new steps
+Given /I have added "(.*)" with rating "(.*)"/ do |title, rating|
+  Given %Q{I am on the Create New Movie page}
+  When  %Q{I fill in "Title" with "#{title}"}
+  And   %Q{I select "#{rating}" from "Rating"}
+  And   %Q{I press "Save Changes"}
+end
+
+Then /I should see "(.*)" before "(.*)" on (.*)/ do |string1, string2, path|
+  Given %Q{I am on #{path}}
+  regexp = Regexp.new ".*#{string1}.*#{string2}"
+  page.body.should =~ regexp
+end
+
 # Single-line step scoper
 When /^(.*) within (.*[^:])$/ do |step, parent|
   with_scope(parent) { When step }
